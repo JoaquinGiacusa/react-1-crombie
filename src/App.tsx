c228deeafa73f3084f49fc27140e63e5fcf72431
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-10-14 00:15:18 -0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,59 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   const [number, setNumber] = useState<number>(0);
 
+  // const increase = () => {
+  //   if (number >= 0 && number <= 9) {
+  //     setNumber(number + 1);
+  //   }
+  // };
+
   const increase = () => {
-    if (number >= 0 && number <= 9) {
-      setNumber(number + 1);
-    }
+    setNumber((prev) => {
+      return prev + 1;
+    });
   };
 
   const decrease = () => {
-    if (number > 0 && number <= 10) {
-      setNumber(number - 1);
-    }
+    setNumber((prev) => {
+      return prev - 1;
+    });
   };
 
-  /*   const increaseAsync = () => {
-    setTimeout(() => {
-      setNumber((currentNumber) => currentNumber + 1);
-    }, 2000);
-  }; */
-
   return (
-    <div style={{ margin: "0 auto", border: "3px solid gray", width: "15em" }}>
-      <button
-        className="btn increase"
-        onClick={increase}
-        disabled={number == 10 ? true : false}
-      >
-        Sumar +
-      </button>
-      <button
-        className="btn descrease"
-        onClick={decrease}
-        disabled={number == 0 ? true : false}
-      >
-        Decrese
-      </button>
-      {/* <button onClick={increaseAsync}>Increase Async</button> */}
+    <div className="container">
+      <h1 className="title">Contador</h1>
+      <div className="btn-container">
+        <button
+          className={`btn ${number == 10 ? "error" : ""}`}
+          onClick={increase}
+          disabled={number == 10 ? true : false}
+        >
+          Sumar +
+        </button>
+        <button
+          className={`btn ${number == 0 ? "error" : ""}`}
+          onClick={decrease}
+          disabled={number == 0 ? true : false}
+        >
+          Restar -
+        </button>
+      </div>
       <h1 className={`counter ${number == 10 || number == 0 ? "red" : ""}`}>
         {number}
       </h1>
+      <img
+        className="refresh"
+        src="./refresh.svg"
+        alt=""
+        width={"20px"}
+        onClick={() => {
+          setNumber(0);
+        }}
+      />
     </div>
   );
 }
