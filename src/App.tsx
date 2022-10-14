216d6a23e35c869b8b9fe7c2888ba1a5ed6b7e99
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-10-13 21:46:57 -0300

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState<number>(0);
+
+  const increase = () => {
+    if (number >= 0 && number <= 9) {
+      setNumber(number + 1);
+    }
+  };
+
+  const decrease = () => {
+    if (number > 0 && number <= 10) {
+      setNumber(number - 1);
+    }
+  };
+
+  /*   const increaseAsync = () => {
+    setTimeout(() => {
+      setNumber((currentNumber) => currentNumber + 1);
+    }, 2000);
+  }; */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "0 auto", border: "3px solid gray", width: "15em" }}>
+      <button
+        className="btn increase"
+        onClick={increase}
+        disabled={number == 10 ? true : false}
+      >
+        Sumar +
+      </button>
+      <button
+        className="btn descrease"
+        onClick={decrease}
+        disabled={number == 0 ? true : false}
+      >
+        Decrese
+      </button>
+      {/* <button onClick={increaseAsync}>Increase Async</button> */}
+      <h1 className={`counter ${number == 10 || number == 0 ? "red" : ""}`}>
+        {number}
+      </h1>
     </div>
   );
 }
